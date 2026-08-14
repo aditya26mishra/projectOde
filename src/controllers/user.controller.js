35c12d508230b756c5315a9fae315f149448e2f4
aditya <adirubics@gmail.com>
2026-08-14 15:55:44 +0530

    added user registration controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,58 @@
 import { asyncHandler } from "../utils/asyncHandler.utils.js";
+import { ApiError } from "../utils/ApiError.utils.js"
+import { ApiResponse } from "../utils/ApiResponse.utils.js"
+import { User } from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.utils.js"
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "ok"
+    const {fullname, email, username, password} = req.body;
+
+    if([fullname, email, username, password].some((field)=>field?.trim() === "")){
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existingUser = User.findOne({
+        $or: [{username}, {email}]
     })
+    
+    if(existingUser){
+        throw new ApiError(409, "User with email or username already exists");
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is required");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+    
+    if(!avatar){
+        throw new ApiError(400, "Avatar file is required");
+    }
+
+    const user = await User.create({
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    });
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong while registering the user");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "user registered successfully")
+    );
 });
 
 export {
